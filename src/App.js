b022309b14b72e92751f5480be760df66df9c350
MattB09 <mattbancroft03@gmail.com>
2021-05-06 17:02:48 +0900

    merge conflict.

--- src/App.js
+++ src/App.js
@@ -5,78 +5,9 @@ import Home from "./components/Home";
 import { AuthProvider } from "./Auth";
 import PrivateRoute from "./PrivateRoute";
 import Landing from "./components/Landing";
-import { addSorted } from './utils/helpers'
+import reducer from "./utils/reducer";
 
 export const Context = React.createContext();
-
-// declare reducer
-const reducer = (state, action) => {
-  switch (action.type) {
-    // set initial context reducer
-    case 'setContext':
-      return { ...state, data: action.payload };
-    // Filter reducers
-    case 'clearFilters':
-      return {...state, filters: {author: "", book: "", tag: ""}}
-    case 'setFilter':
-      return {...state, filters: action.payload}
-    // Author reducers
-    case 'addAuthor':
-      let authorsCopy = [...state.data.authors];
-      addSorted(action.payload, authorsCopy, "name");
-      return { filters: state.filters, data: { ...state.data, authors: authorsCopy}}
-    case 'editAuthor':
-      let authorsCopy1 = [...state.data.authors];
-      let ind = authorsCopy1.findIndex((auth) => auth.id === action.payload.id);
-      authorsCopy1.splice(ind, 1, action.payload);
-      return { ...state, data: { ...state.data, authors: authorsCopy1}}
-    case 'deleteAuthor':
-      let authorsCopy2 = [...state.data.authors];
-      let index = authorsCopy2.findIndex((auth) => auth.id === action.payload.id);
-      authorsCopy2.splice(index, 1);
-      return { ...state, data: { ...state.data, authors: authorsCopy2}}
-    // Books reducers
-    case 'addBook':
-      let booksCopy = [...state.data.books];
-      addSorted(action.payload, booksCopy, "title");
-      return { filters: state.filters, data: { ...state.data, books: booksCopy}}
-    case 'deleteBook':
-      let booksCopy1 = [...state.data.books];
-      let hCopy = [...state.data.highlights].filter(h => h.book_id !== action.payload.id);
-      let bIndex = booksCopy1.findIndex((book) => book.id === action.payload.id);
-      booksCopy1.splice(bIndex, 1);
-      return { ...state, data: {...state.data, highlights: hCopy, books: booksCopy1}};
-    // tags reducers
-    case 'addTag':
-      let tagsCopy = [...state.data.tags];
-      addSorted(action.payload, tagsCopy, "tag");
-      return { filters: state.filters, data: {...state.data, tags: tagsCopy}};
-    case 'deleteTag':
-      // remove tag from highlights
-      let tagsCopy1 = [...state.data.tags];
-      let hCopy1 = [...state.data.highlights];
-      hCopy1.forEach((h) => {
-        let index = h.tags.findIndex(tag => tag.id === action.payload.id);
-        if (index > -1) {
-          h.tags.splice(index, 1);
-        }
-      })
-      let tIndex = tagsCopy1.findIndex((tag) => tag.id === action.payload.id);
-      tagsCopy1.splice(tIndex, 1);
-      return { ...state, data: {...state.data, highlights: hCopy1, tags: tagsCopy1}};
-    case 'addHighlight':
-      let highlightsCopy = [...state.data.highlights];
-      addSorted(action.payload, highlightsCopy, "highlight");
-      return { filters: state.filters, data: {...state.data, highlights: highlightsCopy}};
-    case 'deleteHighlight':
-      let highlightsCopy1 = [...state.data.highlights];
-      let hIndex = highlightsCopy1.findIndex((h) => h.id === action.payload.id);
-      highlightsCopy1.splice(hIndex, 1);
-      return { filters: state.filters, data: {...state.data, highlights: highlightsCopy1}};
-    default:
-      return state
-  }
-}
 
 function App() {
   const [state, dispatch] = useReducer(reducer, {filters: {author: "", book: "", tag: ""}});
